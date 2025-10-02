3168467271b14bf333fb965009abba71a7fba7c9
Kiphea <djsmyth05@gmail.com>
2025-10-02 16:35:06 +0100

    Task 1 Complete plus other Task Admin done

--- FirstPractical/Program.cs
+++ FirstPractical/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Task1();
+//Task2();
+//Task3();
+//Task4();
+//Task5();
+
+void Task1()
+{
+    // Collects the users name and the age they will be in 5 years. Then prints it
+    Console.WriteLine("Please enter your name.");
+    string userName = Console.ReadLine();
+
+    Console.WriteLine("Please enter your age.");
+    int userAgeIn5Years = Convert.ToInt32(Console.ReadLine()) + 5;
+
+    Console.WriteLine($"Hello {userName}, you will be {userAgeIn5Years} in 5 years.");
+}
+
+void Task2()
+{
+
+}
+
+void Task3()
+{
+
+}
+
+void Task4()
+{
+
+}
+
+void Task5()
+{
+
+}
