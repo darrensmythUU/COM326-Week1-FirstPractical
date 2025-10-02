e412b812e526bfb739ac3dea7b4fd92f9636cc16
Kiphea <djsmyth05@gmail.com>
2025-10-02 17:03:22 +0100

    Task 2 Complete

--- FirstPractical/Program.cs
+++ FirstPractical/Program.cs
@@ -1,5 +1,5 @@
-﻿Task1();
-//Task2();
+﻿//Task1();
+Task2();
 //Task3();
 //Task4();
 //Task5();
@@ -18,7 +18,30 @@ void Task1()
 
 void Task2()
 {
+    // Collects the users age, determines whether they are a child, teenager or adult, then prints it to the console.
+    Console.WriteLine("Please enter your age.");
+    int userAge = Convert.ToInt32(Console.ReadLine());
 
+    if (userAge > 110)
+    {
+        Console.WriteLine($"The user's age of {userAge} is not valid due to being over the maximum age limit of 110.");
+    }
+    else if (userAge < 0)
+    {
+        Console.WriteLine($"The user's age of {userAge} is not valid due to being under the minimum age limit of 0.");
+    }
+    else if (userAge > 19)
+    {
+        Console.WriteLine($"With an age of {userAge}, this user is an adult.");
+    }
+    else if (userAge >= 13)
+    {
+        Console.WriteLine($"With an age of {userAge}, this user is a teenager.");
+    }
+    else
+    {
+        Console.WriteLine($"With an age of {userAge}, this user is a child.");
+    }
 }
 
 void Task3()
